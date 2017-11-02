3b5292f0b604dd09718ad7ad3b3e529d16d08f2f
ainsleybc <ab.chang@me.com>
2017-11-02 19:32:36 +0000

    updated tests to follow business logic

--- test/features/loginSpec.js
+++ test/features/loginSpec.js
@@ -6,32 +6,35 @@ describe('Log in', function() {
 
   const browser = new Browser();
 
-  before(function(done) {
+  before(function (done) {
     this.server = http.createServer(app).listen(3001);
+    browser.visit('/users/new', done);    
+  });
+
+  before(function(done) {
+    browser
+      .fill('username', 'Hugo')
+      .fill('email', 'chris@hacc.com')
+      .fill('password', 'hello123')
+      .fill('passwordConfirmation', 'hello123')
+      .pressButton('Sign Me Up!', done);
+  });
+
+  before(function(done) {
     browser.visit('/login', done);
   });
 
-  after(function(done) {
-    this.server.close();
-    mongoose.connection.db.dropDatabase(done);
+  before(function(done) {
+    browser
+      .fill('username', 'Hugo')
+      .fill('password', 'hello123')
+      .pressButton('Login', done);
   });
 
-  describe("User should be able to log in", function() {
-    before(function(done) {
-      var user = new User({
-        username: 'Hugo',
-        email: 'email@email.email',
-        password: 'hello123'
-      });
-      browser
-        .fill('username', 'Hugo')
-        .fill('password', 'hello123')
-        .pressButton('Login');
-      done();
-    });
-
+  describe("User should be able to log in", function () {
+    
     it('expects to be able to able to log in', function() {
-      expect(browser.url).to.equal('http://makers-bnb.com/home');
+      expect(browser.url).to.equal('http://makers-bnb.com/Hugo/spaces');
     });
   
   });
